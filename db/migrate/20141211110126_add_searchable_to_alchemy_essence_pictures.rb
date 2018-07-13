9ff908cd054a06dcd9f2b36ce81835eae6f70fc8
Christoph Eicke <eicke@yfu.de>
2018-07-13 14:36:12 +0200

    Update migrations

--- db/migrate/20141211110126_add_searchable_to_alchemy_essence_pictures.rb
+++ db/migrate/20141211110126_add_searchable_to_alchemy_essence_pictures.rb
@@ -1,4 +1,4 @@
-class AddSearchableToAlchemyEssencePictures < ActiveRecord::Migration
+class AddSearchableToAlchemyEssencePictures < ActiveRecord::Migration[4.2]
   def change
     add_column :alchemy_essence_pictures, :searchable, :boolean, default: true
   end
